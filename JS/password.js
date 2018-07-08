561b6e96236a05fce31612e0fd0df25c8484ff7f
Unknown <mgijon94@gmail.com>
2018-07-08 19:02:37 +0200

    updated

--- JS/password.js
+++ JS/password.js
@@ -1,9 +1,8 @@
 var password;
 var correcta = '12345';
-password = prompt('Enter password to access', '');
+//password = prompt('Enter password to access', '');
+password = document.createElement("INPUT");
+password.setAttribute("type", "text")
 if (password == correcta){
   alert('Correct!! Enjoy the graph');
-}
-else{
-  window.location="http://thegraph.es"
 }

d9bdf7bb6c44275aaccba24e8ed81fa0d81b3f68
Unknown <mgijon94@gmail.com>
2018-07-15 14:05:48 +0200

    updated

---
+++ php/recibe.php
@@ -0,0 +1,12 @@
+<?php
+
+print_r($_POST);
+
+$user = $_POST['user'];
+$mail = $_POST['mail'];
+$password = $_POST['password'];
+$password_2 = $_POST['password_2'];
+$age = $_POST['age'];
+
+// bla bla bla bla
+>

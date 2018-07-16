c5ded49b8e438da964342b580b7de48dec7b1a2c
Unknown <mgijon94@gmail.com>
2018-07-16 22:26:46 +0200

    updated

--- php/recibe.php
+++ php/recibe.php
@@ -1,6 +1,12 @@
 <?php
 
-print_r($_POST);
+//print_r($_POST);
+
+// si usamos el método get enviaremos los valores por url
+if (!$_POST) {
+  // Estamos redirigiendo, lo ideal sería hacerlo hacia nuestro formulario
+  header('Location: http://www.google.com')
+}
 
 $user = $_POST['user'];
 $mail = $_POST['mail'];
--- php/validation.php
+++ php/validation.php
@@ -1,5 +1,10 @@
 <?php
 
+if (!$_POST) {
+  // redireccionamos si no llegan los datos
+  header('Location: https://www.google.com')
+}
+
 $user = $_POST['user']; // debo buscar si existe en la base de datos y si existe pasare a comprobar la contraseña
 $pass = $_POST['password'];
 

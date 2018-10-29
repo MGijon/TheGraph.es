e79c0a0fa98ddf48acc05f3b1d5e408dc266b2dc
Unknown <mgijon94@gmail.com>
2018-10-29 10:18:44 +0100

    Nueva estructura

--- JS/password.js
+++
@@ -1,8 +0,0 @@
-var password;
-var correcta = '12345';
-//password = prompt('Enter password to access', '');
-password = document.createElement("INPUT");
-password.setAttribute("type", "text")
-if (password == correcta){
-  alert('Correct!! Enjoy the graph');
-}

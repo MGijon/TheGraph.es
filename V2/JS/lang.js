ccd02d94d9c10145f17ba610a5e8363edbe80feb
Unknown <mgijon94@gmail.com>
2019-04-01 19:42:13 +0200

    updated

---
+++ V2/JS/lang.js
@@ -0,0 +1,10 @@
+function changetoSpanish() {
+  document.getElementById("spanish-button").style.color = "blue";
+}
+        
+function changetoEnglish(){
+    document.getElementById("english-button").style.color = "green";
+}
+
+changetoEnglish()
+changetoSpanish()

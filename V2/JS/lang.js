b05937e92e1657fb79e9723722b245226c485d85
Unknown <mgijon94@gmail.com>
2019-04-04 20:08:43 +0200

    updated

--- V2/JS/lang.js
+++ V2/JS/lang.js
@@ -1,20 +1,23 @@
-const introduction_text_english = "hello world"
+const introduction_text= "hello world"
 // Inicialmente estará en inglés, decalro como constantes todos los textos y me los guardo, luego creo las traducciones y las 
 // escribo como constantes aquí, el botón hará el cambio entre ambos -> solo necesitaré una función para cambiar de idioma!!
 
-function changetoSpanish() {
-    document.getElementById("spanish-button").onclick = function () {
-        texto_introduction = document.getElementById("introduction-text");
-        texto_introduction.innerHTML = introduction_text_english;
+function language() {
+    texto_introduction = document.getElementById("introduction-text");
+    
+    document.getElementById("spanish-button").onclick = function () {    
+        texto_introduction.innerHTML = introduction_text;
+    };
+    
+    document.getElementById("english-button").onclick = function() {
+        texto_introduction.innerHTML = "alright motherfucker!!"  
     };
 }
 
-function changetoEnglish() {
-    document.getElementById("english-button").style.color = "green";
-}
 
-changetoEnglish()
-changetoSpanish()
+language()
 
 // https://www.w3schools.com/js/js_whereto.asp
+// https://www.w3schools.com/jsref/event_onclick.asp
+// https://www.w3schools.com/js/js_hoisting.asp
 // https://www.w3schools.com/jsref/event_onclick.asp

151c59e8f9f6433a2c518635e02c6f744a592809
Unknown <mgijon94@gmail.com>
2020-01-13 22:36:01 +0100

    Working on visualizations for post 1

--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRBokeh.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRBokeh.py
@@ -1,2 +1,26 @@
+"""Linear Regression Const function visualization."""
+# Numpy
 import numpy as np
+# Bokeh libraries
+from bokeh.io import output_file, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import ColumnDataSource
+from bokeh.layouts import row, column, gridplot
+from bokeh.models.widgets import Tabs, Panel
 
+# LINEAR REGRESSION COST FUNCTION
+
+# BOKEH 
+# Determine where the visualization will be rendered
+output_file('LinearRBokeh.html')  # Render to static HTML, or
+output_notebook()  # Render inline in a Jupyter Notebook
+
+# Set up the figure(s)
+fig = figure()  # Instantiate a figure() object
+
+# Connect to and draw the data
+
+# Organize the layout
+
+# Preview and save
+show(fig)  # See what I made, and save if I like it

f3de7a0744bd3254af67c3dcb122a048d19c9e81
Unknown <mgijon94@gmail.com>
2020-01-15 20:08:10 +0100

    Working on visualizations for post 1
    really minimal changes, still dont know how to face it

--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRBokeh.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRBokeh.py
@@ -10,21 +10,24 @@ from bokeh.models.widgets import Tabs, Panel
 
 # LINEAR REGRESSION COST FUNCTION
 #https://ml-cheatsheet.readthedocs.io/en/latest/linear_regression.html
+x = np.random.normal(size=10)
+y = np.random.normal(size=10)
+
 
 # BOKEH
 # Determine where the visualization will be rendered
-output_file('LinearRBokeh.html')  # Render to static HTML, or
+output_file('visualizations/LinearRBokeh.html')  # Render to static HTML, or
 output_notebook()  # Render inline in a Jupyter Notebook
 
 # Set up the figure(s)
 fig = figure()  # Instantiate a figure() object
 
+fig.scatter(x, y)
 # Connect to and draw the data
 
 # Organize the layout
 
 # Preview and save
 show(fig)  # See what I made, and save if I like it
-
 
 # https://realpython.com/python-data-visualization-bokeh/

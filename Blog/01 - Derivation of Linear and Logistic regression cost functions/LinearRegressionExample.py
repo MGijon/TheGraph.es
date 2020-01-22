4eaf464fbecedca40c9ae76621a16a5beeb8eba5
Unknown <mgijon94@gmail.com>
2020-01-22 18:51:30 +0100

    small changes

--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
@@ -1,3 +1,6 @@
 """Example of Linear Regression.
     Just an example of linear regression application.
 """
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LogisticRegressionExample.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LogisticRegressionExample.py
@@ -1,5 +1,7 @@
 """Example of Logistic Regression.
     Just an example of Logistic Regression application.
 """
-
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 # ejemplo, necesito encontrar un dataset

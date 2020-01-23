06c4810c5665dfbcb94fe166960810751851853e
Unknown <mgijon94@gmail.com>
2020-01-23 23:40:11 +0100

    small change

--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
@@ -3,4 +3,8 @@
 """
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
+
+# se trata de hacer simplemente un ejemplo, ajustar la recta y poco más, mostrar los valores
+
+# codificarla por partes, función de pérdida, de coste y gradient descent 

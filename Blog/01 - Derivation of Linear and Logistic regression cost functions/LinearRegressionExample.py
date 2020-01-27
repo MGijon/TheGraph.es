03f302ff9cda276f122bd45fa069acf227e4af99
Unknown <mgijon94@gmail.com>
2020-01-27 23:19:47 +0100

    small changes

--- Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/LinearRegressionExample.py
@@ -73,14 +73,26 @@ plt.show()
 
 ## estaría super bien poder printar las tres rectas de regresion juntas por especie
 
-##
+## ============= ##
 ## LOSS_FUNCTION ##
 ## ============= ##
 
+def loss_function():
+    """Description TODO."""
+    return None
 
-##
+## ============= ##
 ## COST_FUNCTION ##
-##
+## ============= ##
 
-##
+def cost_function():
+    """Description TODO."""
+    return None
+
+## ================ ##
 ## GRADIENT_DESCENT ##
+## ================ ##
+
+def gradient_descent():
+    """Description TODO."""
+    return None

16e0dbd2cb86f10d40e6ac77f668e6a999368a2e
Unknown <mgijon94@gmail.com>
2020-01-29 19:22:32 +0100

    small changes

---
+++ Blog/01 - Derivation of Linear and Logistic regression cost functions/linear_functions.py
@@ -0,0 +1,25 @@
+"""Functions for the Linear Regression."""
+
+## ============= ##
+## LOSS_FUNCTION ##
+## ============= ##
+
+def loss_function():
+    """Description TODO."""
+    return None
+
+## ============= ##
+## COST_FUNCTION ##
+## ============= ##
+
+def cost_function():
+    """Description TODO."""
+    return None
+
+## ================ ##
+## GRADIENT_DESCENT ##
+## ================ ##
+
+def gradient_descent():
+    """Description TODO."""
+    return None

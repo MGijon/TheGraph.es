0d0384a70c2c28310f9f03ed5cbb39b384232dd1
Unknown <mgijon94@gmail.com>
2020-06-25 21:22:33 +0200

    project system mvp working

--- Graph/Graph/projects/views.py
+++ Graph/Graph/projects/views.py
@@ -3,18 +3,16 @@
 # Django
 from django.views import generic
 
+# Project models
 from .models import Project
-'''
-from django.template.response import TemplateResponse
 
-def temporal_function(request):
-    """Temporal, just return the template."""
-    print('\nEntra en esta función temporal ad-hoc\n')
-    return TemplateResponse(request, 'about.html')  #projects/feed_projects.html')
-
-'''
 class ProjectsList(generic.ListView):
     """Post list view."""
-    print('\nentra en la función ProjectsList\n')
     queryset = Project.objects.filter(status=1).order_by('-created_on')
     template_name = 'projects/feed_projects.html'
+
+class ProjectDetail(generic.DetailView):
+    """Project detail view."""
+    model = Project
+
+    template_name = 'projects/project_detail.html'
